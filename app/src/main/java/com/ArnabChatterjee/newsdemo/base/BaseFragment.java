f545017d7776c815a70c31c274136eaa34765e56
ARNABCHATT <48590751+ARNABCHATT@users.noreply.github.com>
2019-07-01 11:16:01 +0530

    Add files via upload

---
+++ app/src/main/java/com/ArnabChatterjee/newsdemo/base/BaseFragment.java
@@ -0,0 +1,9 @@
+package com.arnabchatterjee.newsdemo.base;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+
+public abstract class BaseFragment extends Fragment {
+    public Context _context;
+}
